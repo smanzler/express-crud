01947245f9ab70147922ad5c88700fdc98e9c795
Simon Manzler <simanzler@gmail.com>
2026-01-31 16:32:39 -0500

    removed user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import type { Express } from "express";
-import userRoutes from "./user.routes";
 import authRoutes from "./auth.routes";
 
 export function registerRoutes(app: Express) {
   app.use("/auth", authRoutes);
-  app.use("/users", userRoutes);
 }
